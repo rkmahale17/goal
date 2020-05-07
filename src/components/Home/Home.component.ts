e3820d590c8b31cd0c175dd134a1a394713c3482
Rahul Mahale <rahulmahale172214@gmail.com>
2020-05-07 21:30:14 +0530

    Resolvng Heruku App

--- src/components/Home/Home.component.ts
+++ src/components/Home/Home.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "Home",
-  templateUrl: "./Home.component.html",
-  styleUrls: ["./Home.component.scss"],
+  templateUrl: "Home.component.html",
+  styleUrls: ["Home.component.scss"],
 })
 export class HomeComponent {
   feature = ["Thought Reminder", "Share Pamyat", "Daily Log", "Categorization"];
